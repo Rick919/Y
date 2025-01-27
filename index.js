82f9658c7e4584adb43ff11b32d3c4c1bce91abd
Tariq Alsharif <tariqalsharief@live.com>
2025-01-27 16:51:20 +0300

    fix rendering issue

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //import { tweetsData } from "./data.js";
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
+
+
 const getTweetsData = localStorage.getItem('tweetsData')
 let tweetsData = JSON.parse(getTweetsData)
 
